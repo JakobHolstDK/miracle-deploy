d7068f48bb087d5df41b4f14aba88a6d9a785454
Jakob Holst <lyngknuden@gmail.com>
2022-08-04 09:22:50 +0200

    rhel support

--- installfromiso.sh
+++ installfromiso.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-virt-install --name installtest  --connect http://localhost:8000/ubuntu-20.04.4-desktop-amd64/
+virt-install --name `date +%s`  --location http://egon.miracle.local:8000/rhel-8.6-x86_64-dvd --osinfo detect=on   --initrd-inject ./ks.cfg  --extra-args="ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8" 
+
+
