4f01fced5a13ba769a93fedc8840d5c5ef459afb
Jakob Holst <jho@flemming.openknowit.com>
2023-08-08 02:57:23 -0400

    demo

--- terraform/cmdb/rackreservations.tf
+++ terraform/cmdb/rackreservations.tf
@@ -1,17 +1,5 @@
-resource "netbox_site" "test" {
-  name   = "test"
-  status = "active"
-}
-
-resource "netbox_rack" "test" {
-  name     = "test"
-  site_id  = netbox_site.test.id
-  status   = "active"
-  width    = 10
-  u_height = 40
-}
-resource "netbox_rack_reservation" "test" {
-  rack_id     = netbox_rack.test.id
+resource "netbox_rack_reservation" "rack1reserved" {
+  rack_id     = netbox_rack.rack1.id
   units       = [1, 2, 3, 4, 5]
   user_id     = 1
   description = "my description"
