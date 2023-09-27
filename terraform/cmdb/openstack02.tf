b66b855a1f7dd982dde68abf2dee03ab4c8649a7
Jakob Holst <jakob.holst@knowit.dk>
2023-09-27 11:08:34 +0200

    demo

--- terraform/cmdb/openstack02.tf
+++ terraform/cmdb/openstack02.tf
@@ -49,23 +49,6 @@ resource "netbox_virtual_machine" "thinlincrhel8" {
   })
 }
 
-resource "netbox_virtual_machine" "thinlincrhel7" {
-  count = 1
-  cluster_id   = netbox_cluster.openstack[1].id
-  name         = "thinlinchel7${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat",  "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
-  vcpus        = "2"
-  role_id      = netbox_device_role.server.id
-  tenant_id    = netbox_tenant.knowit.id
-  local_context_data = jsonencode({
-    "operating_system" = "rhel-7"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
-  })
-}
-
 resource "netbox_virtual_machine" "thinlincrhel9" {
   count = 1
   cluster_id   = netbox_cluster.openstack[1].id
