72feba314102dec8cb0006c18e93eba679aecf01
Jakob Holst <jakob.holst@knowit.dk>
2023-09-27 09:56:08 +0200

    new layout

--- terraform/cmdb/openstack03.tf
+++ terraform/cmdb/openstack03.tf
@@ -117,12 +117,10 @@ resource "netbox_virtual_machine" "ckan" {
   })
 }
 
+
 resource "netbox_virtual_machine" "aap" {
   count = 1
   cluster_id   = netbox_cluster.openstack[2].id
-<<<<<<< HEAD
-  name         = "ckan${format("%02d", count.index + 1)}"
-=======
   name         = "aap${format("%02d", count.index + 1)}"
   tags         = [ "os_redhat",  "role_backup" ]
   disk_size_gb = 100
@@ -136,41 +134,4 @@ resource "netbox_virtual_machine" "aap" {
     "activation_key" = "Faceted-Oil-Scion6"
   })
 }
-
-resource "netbox_virtual_machine" "thinlinc" {
-  count = 1
-  cluster_id   = netbox_cluster.openstack[2].id
-  name         = "thinlinc${format("%02d", count.index + 1)}"
->>>>>>> 0e31420 (demon)
-  tags         = [ "os_redhat",  "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
-  vcpus        = "2"
-  role_id      = netbox_device_role.server.id
-  tenant_id    = netbox_tenant.knowit.id
-  local_context_data = jsonencode({
-    "operating_system" = "rhel-9.2"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
-  })
-}
-
-
-resource "netbox_virtual_machine" "thinlinc" {
-  count = 1
-  cluster_id   = netbox_cluster.openstack[2].id
-  name         = "thinlinc${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat",  "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
-  vcpus        = "2"
-  role_id      = netbox_device_role.server.id
-  tenant_id    = netbox_tenant.knowit.id
-  local_context_data = jsonencode({
-    "operating_system" = "rhel-9.2"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
-  })
-}
-
 
