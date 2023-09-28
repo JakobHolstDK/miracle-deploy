4872ae46ce0e52a7f82fafa178e0c073349cd984
Jakob Holst <jakob.holst@knowit.dk>
2023-09-28 14:33:58 +0200

    ubuntu in netbox

--- terraform/cmdb/openstack03.tf
+++ terraform/cmdb/openstack03.tf
@@ -104,16 +104,14 @@ resource "netbox_virtual_machine" "ckan" {
   count = 1
   cluster_id   = netbox_cluster.openstack[2].id
   name         = "ckan${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat",  "role_backup" ]
+  tags         = [ "os_ubuntu",  "role_backup" ]
   disk_size_gb = 100
   memory_mb    = 8192
   vcpus        = "2"
   role_id      = netbox_device_role.server.id
   tenant_id    = netbox_tenant.knowit.id
   local_context_data = jsonencode({
-    "operating_system" = "rhel-9.2"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
+    "operating_system" = "ubuntu-22.04"
   })
 }
 
