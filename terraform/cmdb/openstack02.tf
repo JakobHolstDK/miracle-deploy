d9757b4c35600e704d52e8b171e5b1cc5ad54304
Jakob Holst <jakob.holst@knowit.dk>
2023-10-20 13:50:01 +0200

    demo

--- terraform/cmdb/openstack02.tf
+++ terraform/cmdb/openstack02.tf
@@ -1,10 +1,10 @@
-resource "netbox_virtual_machine" "openshiftmaster" {
+resource "netbox_virtual_machine" "wgtest" {
   count = 3
   cluster_id   = netbox_cluster.openstack[1].id
-  name         = "openshiftmaster${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat", "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
+  name         = "wgtest${format("%02d", count.index + 1)}"
+  tags         = [ "os_redhat",  "role_backup" ]
+  disk_size_gb = 50
+  memory_mb    = 1024
   vcpus        = "2"
   role_id      = netbox_device_role.server.id
   tenant_id    = netbox_tenant.knowit.id
@@ -15,12 +15,13 @@ resource "netbox_virtual_machine" "openshiftmaster" {
   })
 }
 
-resource "netbox_virtual_machine" "openshiftworker" {
+
+resource "netbox_virtual_machine" "perconatest" {
   count = 3
   cluster_id   = netbox_cluster.openstack[1].id
-  name         = "openshiftworker${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat", "role_backup" ]
-  disk_size_gb = 100
+  name         = "perconatest${format("%02d", count.index + 1)}"
+  tags         = [ "os_redhat",  "role_backup" ]
+  disk_size_gb = 50
   memory_mb    = 8192
   vcpus        = "8"
   role_id      = netbox_device_role.server.id
@@ -32,37 +33,4 @@ resource "netbox_virtual_machine" "openshiftworker" {
   })
 }
 
-resource "netbox_virtual_machine" "thinlincrhel8" {
-  count = 1
-  cluster_id   = netbox_cluster.openstack[1].id
-  name         = "thinlinchel8${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat",  "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
-  vcpus        = "2"
-  role_id      = netbox_device_role.server.id
-  tenant_id    = netbox_tenant.knowit.id
-  local_context_data = jsonencode({
-    "operating_system" = "rhel-8"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
-  })
-}
-
-resource "netbox_virtual_machine" "thinlincrhel9" {
-  count = 1
-  cluster_id   = netbox_cluster.openstack[1].id
-  name         = "thinlinchel9${format("%02d", count.index + 1)}"
-  tags         = [ "os_redhat",  "role_backup" ]
-  disk_size_gb = 100
-  memory_mb    = 8192
-  vcpus        = "2"
-  role_id      = netbox_device_role.server.id
-  tenant_id    = netbox_tenant.knowit.id
-  local_context_data = jsonencode({
-    "operating_system" = "rhel-9"
-    "rhel_org" = 6207854
-    "activation_key" = "Faceted-Oil-Scion6"
-  })
-}
 
