a85d3d4879c96236b8da157f0529962d89cea475
Jakob Holst <jakob.holst@knowit.dk>
2024-02-07 13:13:08 +0000

    demo

--- terraform/cmdb/openstack02.tf
+++ terraform/cmdb/openstack02.tf
@@ -24,6 +24,33 @@ resource "netbox_virtual_machine" "wgtest" {
   )
 }
 
+resource "netbox_virtual_machine" "elementor" {
+  count = 1
+  cluster_id   = netbox_cluster.openstack[1].id
+  name         = "elementor${format("%02d", count.index + 1)}"
+  tags         = [ "os_redhat",  "role_backup" ]
+  disk_size_gb = 50
+  memory_mb    = 1024
+  vcpus        = "2"
+  role_id      = netbox_device_role.server.id
+  tenant_id    = netbox_tenant.knowit.id
+  local_context_data = jsonencode(
+        {
+        "os": {
+                "activation_key": "Faceted-Oil-Scion6",
+                "operating_system": "rhel-9.2",
+                "rhel_org": "6207854A"
+        },
+        "wireguard": {
+                "network": "wgdemo",
+                "public_key": "",
+                "role": "client"
+                }
+        }
+  )
+}
+
+
 
 resource "netbox_virtual_machine" "perconatest" {
   count = 3
@@ -90,6 +117,33 @@ resource "netbox_virtual_machine" "dashapp" {
   disk_size_gb = 50
   memory_mb    = 3500
   vcpus        = "1"
+  role_id      = netbox_device_role.server.id
+  tenant_id    = netbox_tenant.knowit.id
+  local_context_data = jsonencode(
+        {
+        "os": {
+                "activation_key": "Faceted-Oil-Scion6",
+                "operating_system": "rhel-9.2",
+                "rhel_org": "6207854A"
+        },
+        "wireguard": {
+                "network": "wgdemo",
+                "public_key": "",
+                "role": "client"
+                }
+        }
+  )
+
+}
+
+resource "netbox_virtual_machine" "elastic" {
+  count = 1
+  cluster_id   = netbox_cluster.openstack[1].id
+  name         = "elastic${format("%02d", count.index + 1)}"
+  tags         = [ "os_redhat",  "role_backup" ]
+  disk_size_gb = 100
+  memory_mb    = 3500
+  vcpus        = "8"
   role_id      = netbox_device_role.server.id
   tenant_id    = netbox_tenant.knowit.id
   local_context_data = jsonencode(
